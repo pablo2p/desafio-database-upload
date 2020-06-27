f01f668b23b7f3409da721b16c4cf0f710978014
Pablo <zkyouta@hotmail.com>
2020-06-26 23:45:23 -0300

    Desafio Concluído

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,58 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Response {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Response): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const categoryRepos = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError("You don't enough balance for this transection", 400);
+    }
+
+    let transectionCategory = await categoryRepos.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transectionCategory) {
+      transectionCategory = categoryRepos.create({
+        title: category,
+      });
+
+      await categoryRepos.save(transectionCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: transectionCategory,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+/* import Transection from '../models/Transaction'; */
+
+import TransectionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const repository = getCustomRepository(TransectionRepository);
+
+    const transection = await repository.findOne(id);
+
+    if (!transection) {
+      throw new AppError('Transection does not exist.', 400);
+    }
+
+    await repository.remove(transection);
   }
 }
 
